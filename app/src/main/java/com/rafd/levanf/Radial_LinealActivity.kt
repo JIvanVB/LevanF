bdf85dc0c8d7f5c0d7d2367aa5aa0114143b9dcc
JIvanVB <darksamus0997@hotmail.com>
2025-03-06 15:42:05 -0700

    detalles

--- app/src/main/java/com/rafd/levanf/Radial_LinealActivity.kt
+++ app/src/main/java/com/rafd/levanf/Radial_LinealActivity.kt
@@ -43,7 +43,7 @@ class Radial_LinealActivity : AppCompatActivity() {
         listView.adapter = tramoAdapter
 
 
-        findViewById<Button>(R.id.generar).setOnClickListener {startActivity(Intent(this,GraphicsResults::class.java))}
+        findViewById<Button>(R.id.generar).setOnClickListener {startActivity(Intent(this,GraphicsActivity::class.java))}
         findViewById<TextView>(R.id.agregarTramos).setOnClickListener {
             tramos.add(0,Tramo())
             tramoAdapter.notifyDataSetChanged()
